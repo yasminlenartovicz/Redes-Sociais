99c57a2cfdcd5eef5dd52bc3bb4c1e5bf5fbe2c4
yasminlenartovicz <151936984+yasminlenartovicz@users.noreply.github.com>
2024-09-27 11:54:24 -0300

    informacoes

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -2,6 +2,9 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pesoas_conectadas} estão conectadas em alguma Rede Social e passa em media ${dados.tempo_medio} horas conectadas.`
+     console.log(dados);
 }
 visualizarInformacoesGlobais()
