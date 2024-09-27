b88b0ceb0684643b3fc0884f37c2779570614eee
yasminlenartovicz <151936984+yasminlenartovicz@users.noreply.github.com>
2024-09-27 12:04:34 -0300

    .

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -5,6 +5,8 @@ async function visualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pesoas_conectadas} estão conectadas em alguma Rede Social e passa em media ${dados.tempo_medio} horas conectadas.`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
      console.log(paragrafo);
 }
 visualizarInformacoesGlobais()
